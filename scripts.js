ba2fc35a17390925b4926e8bdcc6ad385ca9cf80
Kasia Kowalska <k.kowalska68@yahoo.com>
2018-03-06 21:16:41 +0100

    Fix createTweet

--- scripts.js
+++ scripts.js
@@ -15,16 +15,16 @@ function createTweet(input) {
         quoteAuthor = "Unknown author";
     }
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-}
 
-if (tweetText.length > 140) {
-    getQuote();
-} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
-}
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+            $('.quote').text(quoteText);
+            $('.author').text("Author: " + quoteAuthor);
+            $('.tweet').attr('href', tweet);
+    } 
+}  
 $(document).ready(function() {
     getQuote();
     $('.trigger').click(function() {
